189ebc8d3568a57627577ae6746c0f6a9edc4a5c
marvinbrettinger1 <marvinbrettinger1@MacBook-Pro-de-Marvin.local>
2025-05-15 15:17:57 +0200

    Rebuild migrations from scratch

--- migrations/versions/29f3f2ac5ff0_initial_schema.py
+++ migrations/versions/29f3f2ac5ff0_initial_schema.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '29f3f2ac5ff0'
+revision = 'b6090f543767'
 down_revision = None
 branch_labels = None
 depends_on = None
