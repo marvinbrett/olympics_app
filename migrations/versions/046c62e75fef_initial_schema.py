f93272ddaec04f207e17e3098e6aa3e2c2164fd1
marvinbrettinger1 <marvinbrettinger1@MBP-de-Marvin.lan>
2025-05-17 17:06:53 +0200

    Rebuild Alembic migrations: initial schema with is_admin

--- migrations/versions/046c62e75fef_initial_schema.py
+++ migrations/versions/046c62e75fef_initial_schema.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f5bbe4bfc32e'
+revision = '046c62e75fef'
 down_revision = None
 branch_labels = None
 depends_on = None
